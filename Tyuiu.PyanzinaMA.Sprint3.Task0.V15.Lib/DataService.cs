f12de8ebf93e0e68254ea7bf0e8ae370d5ae5950
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-10-21 17:25:14 +0500

    Task0

---
+++ Tyuiu.PyanzinaMA.Sprint3.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PyanzinaMA.Sprint3.Task0.V15.Lib
+{
+    public class DataService : ISprint3Task0V15
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                sumSeries = sumSeries + (((Math.Pow(value, i)) + (2 / (i + 1))) * Math.Sin(value));
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
