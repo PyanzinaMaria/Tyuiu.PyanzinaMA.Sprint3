ade4118b3d42a39e07f4748a4ee39fb902b63131
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-10-28 23:36:42 +0500

    Task7

---
+++ Tyuiu.PyanzinaMA.Sprint3.Task7.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint3.Task7.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint3.Task7.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0eb86a5c-f736-4c48-8fad-c1989e74814b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
